f5a5b7d8af225317f5b80bbf7adf276c72ba473b
gooseberry <gooseberry@users.noreply.github.com>
2022-09-19 10:21:24 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -113,10 +113,10 @@ main () {
   echo
 
   update_system
-  install_packages ${REQUIRED_PACKAGES[@]}
-  install_ansbible
-  append_path ${PATH_DIRECTORIES[@]}
-  clone_repos ${REPO_URL[@]}
+  #install_packages ${REQUIRED_PACKAGES[@]}
+  #install_ansbible
+  #append_path ${PATH_DIRECTORIES[@]}
+  #clone_repos ${REPO_URL[@]}
 
   echo
   echo "************************************************************"

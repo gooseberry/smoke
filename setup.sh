e94d11bb4155cba393524354e6afb0b152ebcd76
gooseberry <eric.desgroseilliers@gmail.com>
2022-10-03 20:44:58 -0400

    Updating install guide

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install python3-pip
 export PATH=$PATH:~/.local/bin
 python3 -m pip install --user ansible
-mkdir ~/git
+mkdir -p ~/git
 cd ~/git
 git clone https://github.com/gooseberry/smoke.git

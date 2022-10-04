7904beaa1fff97a9517d1ccca1e4b0f05a7a086f
gooseberry <gooseberry@users.noreply.github.com>
2022-10-04 06:27:02 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install python3-pip
+sudo apt-get install python3-pip -y
 export PATH=$PATH:~/.local/bin
 python3 -m pip install --user ansible
 mkdir -p ~/git
